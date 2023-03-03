b26423cad222a16563f8ea34593b9bafa943e7e6
kbendimerad <kamel.bendimerad@gmail.com>
2023-03-03 13:08:45 +0100

    adding downloadPdf service

---
+++ src/app/shared/services/collaborator/collaborator.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
+import { CollaboratorService } from './collaborator.service';
+
+describe('CollaboratorService', () => {
+  let service: CollaboratorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+
+    });
+    service = TestBed.inject(CollaboratorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get collaborator', () =>  {
+    var result = service.getCollaborator(11);
+  });
+});
